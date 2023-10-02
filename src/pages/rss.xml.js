d3e7b9b40a79b3fd3f7c991523a2348aeb05f7cd
Azul <azul235arangui@gmail.com>
2023-10-02 15:46:50 -0300

    add rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Astro alumnaSFDSASDSA | Blog',
+    title: 'Astro alumno | Blog',
     description: 'Mi viaje de aprendizaje de Astro',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
